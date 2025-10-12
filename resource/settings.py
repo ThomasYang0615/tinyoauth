4510d6c6a7be1b354811eb7f1b192a0322f7c315
Thomas Yang <thomas1111177@gmail.com>
2025-10-12 21:33:56 +0800

    chore: fixed typo in resource/settings.py

--- resource/settings.py
+++ resource/settings.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     AUTH_ISSUER: str = "http://auth:8000"
     JWKS_URL: str = "http://auth:8000/.well-known/jwks.json"
-    AUDIENCD: str = "notes-api"
+    AUDIENCE: str = "notes-api"
 
     class Config:
         env_file = ".env"
